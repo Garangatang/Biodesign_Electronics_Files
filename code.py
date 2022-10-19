26db85dcf5835eac20e08f7aaa15adcd27e29295
Garangatang <47489148+Garangatang@users.noreply.github.com>
2022-10-19 16:18:50 -0600

    Working buzzer trigger

--- code.py
+++ code.py
@@ -6,16 +6,21 @@ import displayio
 import terminalio
 import adafruit_displayio_ssd1306
 from adafruit_display_text import label
+import pwmio
+import time
+import gc
+
+gc.enable()
 
 # Counter for input weight to alarm at
 counter = 0;
 
 # Initializing all buttons and their pull up resistors.
-button_input1 = digitalio.DigitalInOut(board.D0)
-button_input2 = digitalio.DigitalInOut(board.D1)
-button_input3 = digitalio.DigitalInOut(board.D2)
-button_input4 = digitalio.DigitalInOut(board.D3)
-button_input5 = digitalio.DigitalInOut(board.D7)
+button_input1 = digitalio.DigitalInOut(board.D10)
+button_input2 = digitalio.DigitalInOut(board.D9)
+button_input3 = digitalio.DigitalInOut(board.D8)
+button_input4 = digitalio.DigitalInOut(board.D7)
+button_input5 = digitalio.DigitalInOut(board.D6)
 
 # Button.fell == True / Pushed
 button_input1.switch_to_input(pull = digitalio.Pull.UP)
@@ -30,42 +35,27 @@ button3 = Debouncer(button_input3)
 button4 = Debouncer(button_input4)
 button5 = Debouncer(button_input5)
 
+# Setting up pulse waves for buzzer
+piezo = pwmio.PWMOut(board.D2, duty_cycle=0, frequency=440, variable_frequency=True)
+
 # Setting up OLED
 displayio.release_displays()
 i2c = busio.I2C (scl=board.SCL, sda=board.SDA)
 
 display_bus = displayio.I2CDisplay (i2c, device_address = 0x3C) # The address of my Board
 
-display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=128, height=64)
-splash = displayio.Group() # no max_size needed
-display.show(splash)
-
-color_bitmap = displayio.Bitmap(128, 64, 1) # Full screen white
-color_palette = displayio.Palette(1)
-color_palette[0] = 0xFFFFFF  # White
- 
-bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
-splash.append(bg_sprite)
- 
-# Draw a smaller inner rectangle
-inner_bitmap = displayio.Bitmap(118, 54, 1)
-inner_palette = displayio.Palette(1)
-inner_palette[0] = 0x000000  # Black
-inner_sprite = displayio.TileGrid(inner_bitmap, pixel_shader=inner_palette, x=5, y=4)
-splash.append(inner_sprite)
-
-# Home screen
-text = "Current Weight"
-text_area = label.Label(terminalio.FONT, text=text, color=0xFFFF00, x=28, y=15)
-splash.append(text_area)
+oled = adafruit_displayio_ssd1306.SSD1306(display_bus, width=128, height=64)
+#Draw a smaller inner rectangle
 
 current_counter = 0
 
+font = terminalio.FONT
+
 while True:
-    if (counter != current_counter):
-        text = "Current Weight"
-        text_area = label.Label(terminalio.FONT, text=text, color=0xFFFF00, x=28, y=15)
-        splash.append(text_area)
+    #if (counter != current_counter):
+    #    text = "Current Weight"
+    #    text_area = label.Label(terminalio.FONT, text=text, color=0xFFFF00, x=28, y=15)
+    #   splash.append(text_area)
     
     button1.update()
     button2.update()
@@ -109,18 +99,44 @@ while True:
 
     if button5.rose:
         print("Button5 released")
+   
+        
+    if (counter == 10):
+        for f in (4500, 2700, 4500):
+            piezo.frequency = f
+            piezo.duty_cycle = 65535 // 2  # On 50%
+            time.sleep(0.25)  # On for 1/4 second
+            piezo.duty_cycle = 0  # Off
+            time.sleep(0.05)  # Pause between notes
+        counter -= 1
     
-    if (counter != current_counter):
-        # Blank slate
-        #display.fill(1)
-        #display.show()
-        text1 = ""
-        text_area1 = label.Label(terminalio.FONT, text=text1, color=0xFFFF00, x=32, y=25)
-        splash.append(text_area1)
-        # Show counter
-        #text2 = "\t"+str(counter)
-        #text_area2 = label.Label(terminalio.FONT, text=text2, color=0xFFFF00, x=32, y=25)
-        #splash.append(text_area2)
-        #current_counter = counter
+    text_display1 = "Current Weight\n"
+    text_display2 = str(counter)
+
+    #clock = label.Label(font, text=time_display)
+    #date = label.Label(font, text=date_display)
+    text1 = label.Label(font, text=text_display1)
+    text2 = label.Label(font, text = text_display2)
+
+    (_, _, width, _) = text1.bounding_box
+    text1.x = oled.width // 2 - width // 2
+    text1.y = 25
+    
+    (_, _, width, _) = text2.bounding_box
+    text2.x = oled.width // 2 - width // 2
+    text2.y = 35
+
+    watch_group = displayio.Group()
+    #watch_group.append(clock)
+    #watch_group.append(date)
+    watch_group.append(text1)
+    watch_group.append(text2)
+
+    oled.show(watch_group)
+  
+    
+    #time.sleep(0.5)
+    #gc.collect()
+        
 
 
